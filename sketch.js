a8b9d5571caf6808f6c1d5cd7e08dafa6f3df376
Lykke1027 <call34632435@gmail.com>
2025-05-12 10:57:49 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let facemesh;
 let video;
 let predictions = [];
 const points = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+const leftEyePoints = [133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155]; // 左眼周圍
+const rightEyePoints = [263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249]; // 右眼周圍
 
 function setup() {
   const canvas = createCanvas(640, 480); // 修改畫布大小為 640x480
@@ -31,22 +33,43 @@ function drawFacemesh() {
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
 
+    // 繪製嘴唇
     stroke(255, 0, 0); // 紅色線條
-    strokeWeight(5);   // 線條粗細
+    strokeWeight(15);   // 線條粗細
     noFill();
-
     beginShape();
     for (let i = 0; i < points.length; i++) {
       const index = points[i];
       if (keypoints[index]) {
         const [x, y] = keypoints[index];
-        console.log(`Point ${index}: (${x}, ${y})`); // 檢查每個點的座標
         vertex(x, y);
-      } else {
-        console.log(`Invalid point index: ${index}`);
       }
     }
-    endShape(CLOSE); // 將最後一點與第一點連接
+    endShape(CLOSE);
+
+    // 繪製左眼周圍
+    stroke(0, 255, 0); // 綠色線條
+    beginShape();
+    for (let i = 0; i < leftEyePoints.length; i++) {
+      const index = leftEyePoints[i];
+      if (keypoints[index]) {
+        const [x, y] = keypoints[index];
+        vertex(x, y);
+      }
+    }
+    endShape(CLOSE);
+
+    // 繪製右眼周圍
+    stroke(0, 0, 255); // 藍色線條
+    beginShape();
+    for (let i = 0; i < rightEyePoints.length; i++) {
+      const index = rightEyePoints[i];
+      if (keypoints[index]) {
+        const [x, y] = keypoints[index];
+        vertex(x, y);
+      }
+    }
+    endShape(CLOSE);
   }
 }
 
